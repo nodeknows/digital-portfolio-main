c77e95a9a7d8856ab2db1c3832ffdc2bcc91fe70
Yang Evan <623469@cart.ads>
2024-05-01 09:43:29 -0700

    .

--- scripts/index.js
+++ scripts/index.js
@@ -2,8 +2,6 @@ function delay(ms) { // await delay(2000)
     return new Promise(resolve => setTimeout(resolve, ms));
 };
 
-let id;
-
 let div = document.querySelector('.intro > div');
 let h1 = document.querySelector('.intro > div > h1');
 let h2 = document.querySelector('.intro > h2');
@@ -21,7 +19,7 @@ async function init() {
 
     h2.style.opacity = 1;
 
-    id = setInterval(() => {
+    setInterval(() => {
         
         if (h2.style.opacity == 1) {
             h2.style.opacity = 0;
@@ -35,7 +33,6 @@ document.querySelector('body').onclick = goHome;
 
 function goHome() {
     window.location = 'home.html'
-    clearInterval(id)
 }
 
 init();
