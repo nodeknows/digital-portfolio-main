52ec0dea58493c5bac7ead6cb1e25069e28fddb8
Yang Evan <623469@cart.ads>
2024-05-08 09:49:20 -0700

    .

--- scripts/home.js
+++ scripts/home.js
@@ -3,18 +3,40 @@ function delay(ms) { // await delay(2000)
 };
 
 let intro = document.querySelector('.intro')
+let trueBody = document.querySelector('body')
+
 let evan = document.querySelector('#evan')
 let yangs = document.querySelector('#yangs')
 let portfolio = document.querySelector('#portfolio')
+
 let body = document.querySelector('.body')
+let p1 = document.querySelector('.body p:nth-of-type(1)')
+let bodyDiv = document.querySelector('.body div')
+let p2 = document.querySelector('.body p:nth-of-type(2)')
+
+let worked = document.querySelector('.worked')
 
 async function init() {
+    window.scrollTo(0, 0);
+
     yangs.style.animationDelay = '.5s'
     portfolio.style.animationDelay = '1s'
 
     evan.setAttribute('transition-style', 'in:wipe:right')
     yangs.setAttribute('transition-style', 'in:wipe:left')
     portfolio.setAttribute('transition-style', 'in:wipe:right')
+
+    p1.style.animationDelay = '3500ms'
+    p1.setAttribute('transition-style', 'in:wipe:top-left')
+    
+
+    bodyDiv.style.animationDelay = '4000ms'
+    bodyDiv.setAttribute('transition-style', 'in:wipe:top-right')
+   
+
+    p2.style.animationDelay = '4500ms'
+    p2.setAttribute('transition-style', 'in:wipe:top-left')
+    
 
     intro.style.transition = 'all 1s'
 
@@ -36,11 +58,11 @@ async function init() {
 
     intro.style.maxHeight = '415px'
 
-    await delay(1000)
+    await delay(2000)
 
-    body.style.opacity = '1'
-
+    worked.style.display = 'flex'
+    trueBody.style.overflowY = 'visible'
 }
 
-
 init()
+
